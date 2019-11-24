7d425927f20c73e490efa502b2db168cd25c30ae
Ines Akrap <akrap.ines@gmail.com>
2019-11-24 14:07:59 +0100

    Cleanup

--- leetcode/easy/1047.js
+++ leetcode/easy/1047.js
@@ -14,3 +14,21 @@ const removeDuplicates = (S) => {
     };
     return newString;
   };
+
+  // V2: stack solution
+// Runtime: 72ms -> 84%
+// Memory: 42.7MB -> 100%
+
+const removeDuplicates = (S) => {
+  const stack = [S[0]];
+  let i = 1;
+  while (i<S.length) {
+    if (stack[stack.length -1] === S[i]) {
+      stack.pop();
+    } else {
+      stack.push(S[i]);
+    }
+    ++i;
+  }
+  return stack.join('');
+}
