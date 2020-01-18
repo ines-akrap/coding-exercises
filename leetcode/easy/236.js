b14e591c00580e96f3705c692fc2cdcac5b6f409
Ines <akrap.ines@gmail.com>
2020-01-18 20:12:18 +0100

    Leetcode 236

--- leetcode/easy/236.js
+++ leetcode/easy/236.js
@@ -21,3 +21,56 @@ const isPalindrome = (head) => {
     }
     return true;
 };
+
+// reverse then compare
+// Runtime: 64ms => 69.83%
+// Memory: 40.6MB => 33.33%
+const isPalindrome = (head) => {
+    if (head === null || head.next === null) {return true;}
+    let reversed = reverseList(head);
+    while (head) {
+        if (head.val !== reversed.val) {return false;}
+        reversed = reversed.next;
+        head = head.next;
+    }
+    return true;
+};
+
+const reverseList = (listHead) => {
+    let reversed = null;
+    while (listHead) {
+        const tempNode = new ListNode(listHead.val);
+        tempNode.next = reversed;
+        reversed = tempNode;
+        listHead = listHead.next;
+    }
+    return reversed;
+}
+
+// reverse, compare half
+// Runtime: 64ms => 70%
+// Memory: 40.6MB => 33.33%
+
+const isPalindrome = (head) => {
+    if (head === null || head.next === null) {return true;}
+    let reversed = reverseList(head);
+    let fast = head;
+    while (fast && fast.next) {
+        if (head.val !== reversed.val) {return false;}
+        reversed = reversed.next;
+        head = head.next;
+        fast = fast.next.next;
+    }
+    return true;
+};
+
+const reverseList = (listHead) => {
+    let reversed = null;
+    while (listHead) {
+        const tempNode = new ListNode(listHead.val);
+        tempNode.next = reversed;
+        reversed = tempNode;
+        listHead = listHead.next;
+    }
+    return reversed;
+}
