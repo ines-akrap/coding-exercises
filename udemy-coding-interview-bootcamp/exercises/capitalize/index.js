9ecc81bceddf157d1aab6bfa7c5386fe99bcb941
Ines <akrap.ines@gmail.com>
2020-01-21 23:20:05 +0100

    S10: Capitalization

--- udemy-coding-interview-bootcamp/exercises/capitalize/index.js
+++ udemy-coding-interview-bootcamp/exercises/capitalize/index.js
@@ -7,6 +7,25 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    let string = str[0].toUpperCase(); // WHAT IF STRING IS SPANISH ?? 
+    for (let i=1; i<str.length; i++) {
+        if (str[i-1] === ' ') {
+            string = string + str[i].toUpperCase();
+        } else {
+            string = string + str[i];
+        }
+    }
+    return string;
+}
+
+// PREFFERED !!!
+function capitalizeS0(str) {
+    const array = [];
+    str.split(' ').forEach((word) => {
+        array.push(word.slice(0,1).toUpperCase() + word.slice(1));
+    });
+    return array.join(' ');
+}
 
 module.exports = capitalize;
