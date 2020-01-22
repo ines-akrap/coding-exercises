cfaba8a95447ca2da1bfffc1b73900fbd0be8ab0
Ines <akrap.ines@gmail.com>
2020-01-22 22:13:26 +0100

    S13: Vowels

--- udemy-coding-interview-bootcamp/exercises/vowels/index.js
+++ udemy-coding-interview-bootcamp/exercises/vowels/index.js
@@ -7,6 +7,24 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let count = 0;
+    for (let char of str.toLowerCase()) {
+        if (isVowel(char)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+function isVowel(char) {
+    const wovels = ['a', 'e', 'i', 'o', 'u'];
+    return (wovels.includes(char));
+}
+
+function vowelsS1(str) {
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
