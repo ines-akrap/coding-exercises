abe73a361496ca356ddc914792443d6d60f126a1
Ines <akrap.ines@gmail.com>
2020-01-26 21:23:54 +0100

    S16: Fibonacci

--- udemy-coding-interview-bootcamp/exercises/fib/index.js
+++ udemy-coding-interview-bootcamp/exercises/fib/index.js
@@ -8,6 +8,25 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// Memoization => O(n)
+function fib(n, map = {0:0, 1:1}) {
+    if (n<2) {return map[n];}
+    return map[n] = fib(n-1, map) + fib(n-2, map); 
+}
+
+// Recursive solution => O(2^n)
+function fibRecursive(n) {
+    if (n < 2) {return n};
+    return fib(n-1) + fib(n-2);
+}
+
+// Iterative solution => O(n)
+function fibIterative(n) {
+    const fibonacci = [0,1];
+    for (let i=2; i<=n; i++) {
+        fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+    }
+    return fibonacci[fibonacci.length-1];
+}
 
 module.exports = fib;
