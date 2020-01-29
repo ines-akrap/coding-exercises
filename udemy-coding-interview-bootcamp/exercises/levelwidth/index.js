fa09fb0738e2c172b341384b62070c694835b59a
Ines <akrap.ines@gmail.com>
2020-01-29 23:20:31 +0100

    S26: Tree width/level width

--- udemy-coding-interview-bootcamp/exercises/levelwidth/index.js
+++ udemy-coding-interview-bootcamp/exercises/levelwidth/index.js
@@ -11,6 +11,20 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const count = [0];
+    const queue = [root,'S'];
+    while(queue.length > 1) {
+        const node = queue.shift();
+        if (node === 'S') {
+            count.push(0);
+            queue.push(node);
+            continue;
+        }
+        queue.push(...node.children);
+        count[count.length-1]++;
+    }
+    return count;
+}
 
 module.exports = levelWidth;
