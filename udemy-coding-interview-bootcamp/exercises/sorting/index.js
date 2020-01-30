da8c7b56c898a8a3dee8f70763c0342ca7e27e2d
Ines <akrap.ines@gmail.com>
2020-01-30 22:57:50 +0100

    S33: Merge sort

--- udemy-coding-interview-bootcamp/exercises/sorting/index.js
+++ udemy-coding-interview-bootcamp/exercises/sorting/index.js
@@ -12,6 +12,7 @@ function bubbleSort(arr) {
     return arr;
 }
 
+// "prove me wrong" alg
 function selectionSort(arr) {
     for (let i=0; i<arr.length; i++) {
         let indexOfMin = i;
@@ -28,11 +29,23 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
-
+    if (arr.length === 1) {
+        return arr;
+    }
+    const midIndex = Math.floor(arr.length / 2);
+    return merge(mergeSort(arr.slice(0,midIndex)), mergeSort(arr.slice(midIndex)));
 }
 
 function merge(left, right) {
-
+    const sorted = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            sorted.push(left.shift());
+        } else {
+            sorted.push(right.shift());
+        }
+    }
+    return [...sorted, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
