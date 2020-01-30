985524abae5e946e4580bab3ad810fd2605b68f7
Ines <akrap.ines@gmail.com>
2020-01-30 22:34:48 +0100

    S32: Selection sort

--- udemy-coding-interview-bootcamp/exercises/sorting/index.js
+++ udemy-coding-interview-bootcamp/exercises/sorting/index.js
@@ -3,7 +3,7 @@
 
 function bubbleSort(arr) {
     for (let i=0; i<arr.length; i++) {
-        for (let j=0; j<arr.length-1; j++) {
+        for (let j=0; j<(arr.length - i - 1); j++) {
             if (arr[j] > arr[j+1]) {
                 [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
             }
@@ -13,7 +13,18 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
-
+    for (let i=0; i<arr.length; i++) {
+        let indexOfMin = i;
+        for (let j=i+1; j<arr.length; j++) {
+            if (arr[j] < arr[indexOfMin]) {
+                indexOfMin = j;
+            }
+        }
+        if (indexOfMin !== i) {
+            [arr[i], arr[indexOfMin]] = [arr[indexOfMin], arr[i]];
+        }
+    }
+    return arr;
 }
 
 function mergeSort(arr) {
