a9d579f0ec109d8a424a18c60f6a9cb3ea99f8aa
Ines <akrap.ines@gmail.com>
2020-05-05 23:01:24 +0200

    Leetcode 307

---
+++ leetcode/medium/307.js
@@ -0,0 +1,39 @@
+// Runtime: 104ms => 63%
+// Memory: 45.3MB => 100%
+
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    this.nums = nums;
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} val
+ * @return {void}
+ */
+NumArray.prototype.update = function(i, val) {
+    this.nums[i] = val;
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(i, j) {
+    let sum = 0;
+    while(i<=j) {
+        sum += this.nums[i];
+        i++;
+    }
+    return sum;
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * obj.update(i,val)
+ * var param_2 = obj.sumRange(i,j)
+ */
